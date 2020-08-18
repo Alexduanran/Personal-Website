924cb9c5e268c33b70dccd039cda88eb35677ec4
Alexduanran <alexduanran@gmail.com>
2020-08-18 14:57:15 -0400

    Add name transition

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import css from './Header.module.css';
 import Main from './Main';
+import { ParallaxProvider, Parallax } from 'react-scroll-parallax';
  
 // function Header() {
 //     return (
@@ -44,7 +45,23 @@ class Header extends React.Component {
         return (
             <div>
                 <div className={css.container} style={{height: this.state.height}}>
-                    Anran Du
+                    <ParallaxProvider>
+                        {/* <Parallax x={[50, -50]} y={[40, -40]} disabled={ this.state.height <= 0.15 * window.innerHeight ? true : false }> */}
+                        <div className={css.name} 
+                            style={{fontSize: this.state.height * 0.06 + 10,
+                                // position: 'absolute',
+                                marginLeft: -window.innerWidth * (1 - this.state.height / window.innerHeight),
+                                marginTop: this.state.height * 0.3,
+                                textAlign: 'center'
+                                // display: 'flex',  
+                                // justifyContent:'center' 
+                                // alignItems:'center'
+                                // height: '100vh',
+                                }}>
+                            <h1>Anran Du</h1>
+                        </div>
+                        {/* </Parallax> */}
+                    </ParallaxProvider>
                 </div>
                 &nbsp;
                 <Main height = {this.state.height}/>
