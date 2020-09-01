c3d5ed8f2875e8d6333be59efa9fddc6c771b13d
Alexduanran <alexduanran@gmail.com>
2020-09-01 01:13:05 -0400

    Add link

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,6 +2,9 @@ import React from 'react';
 import css from './Projects.module.css'
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import GitHubIcon from '@material-ui/icons/GitHub';
 
 import savetrees1 from '../media/savetrees1.png';
 import savetrees2 from '../media/savetrees2.png';
@@ -36,6 +39,12 @@ function Projects() {
                             Created a proof-of-concept website keeping track of paper usage on college campuses to encourage students 
                             and professors to be more environmentally friendly using React.js, React Router, and Google Firestore.
                         </p>
+                        <Button variant="contained" href='https://bcwebcourse.github.io/creative-project-the-savetrees-team/' target="_blank">
+                            Website
+                        </Button>
+                        <IconButton aria-label='github' href='https://github.com/bcwebcourse/creative-project-the-savetrees-team' target="_blank">
+                            <GitHubIcon />
+                        </IconButton>
                     </div>
                     <div className={css.colOne}></div>
                     <div className={css.colFive}>
@@ -58,8 +67,10 @@ function Projects() {
                         <h3>Spring 2020</h3>
                         <p>
                         Developed a web page predicting stock trend based on deep learning using Python, React.js front-end, vis.js, and Flask back- end.
-
                         </p>
+                        <IconButton aria-label='github' href='https://github.com/Alexduanran/Group-8---ATM' target="_blank">
+                            <GitHubIcon />
+                        </IconButton>
                     </div>
                     <div className={css.colOne}></div>
                     <div className={css.colFive}>
@@ -84,6 +95,12 @@ function Projects() {
                         <p>
                         Implemented visualization for traversed paths and debugged the adaptation of Dijkstra’s algorithm.
                         </p>
+                        <Button variant="contained" href='http://dijkstras-arena.herokuapp.com/' target="_blank">
+                            Website
+                        </Button>
+                        <IconButton aria-label='github' href='https://github.com/rogerwangcs/Dijkstras-Arena' target="_blank">
+                            <GitHubIcon />
+                        </IconButton>
                     </div>
                     <div className={css.colOne}></div>
                     <div className={css.colFive}>
@@ -100,10 +117,13 @@ function Projects() {
                 </div>
                 <div className={css.row}>
                     <div className={css.colSix}>
-                        <h3>Spring 2020</h3>
+                        <h3>Fall 2018</h3>
                         <p>
                         Designed an iOS app serving as a platform for online goods exchange among students at Boston College using Swift and Google Firestore.
                         </p>
+                        <IconButton aria-label='github' href='https://github.com/Alexduanran/Eagle-Exchange' target="_blank">
+                            <GitHubIcon />
+                        </IconButton>
                     </div>
                     <div className={css.colOne}></div>
                     <div className={css.colFive}>
