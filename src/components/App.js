4505cfe144334e616ead3a4a54df80a2b16cf0c0
Alexduanran <alexduanran@gmail.com>
2021-01-13 22:06:48 -0500

    Update copyright year

--- src/components/App.js
+++ src/components/App.js
@@ -4,24 +4,28 @@ import Header from './Header';
 import Main from './Main';
 
 function App() {
+	const [data, setData] = useState([]);
 
-  const [data, setData] = useState([]);
+	let months = ['Jan.', 'Feb.', 'Mar.', 'Apr.', 'May', 'June', 'July', 'Aug.', 'Sep.', 'Oct.', 'Nov.', 'Dec.']
+	var d = new Date();
+	var year = d.getFullYear();
+	var month = months[d.getMonth()]
 
-  useEffect(() => {
-    let url = "./data.json";
-    fetch(url)
-    .then((response) => {return response.json()})
-    .then((data) => setData(data));
-  }, [])
-  
-  return (
-    <div className={css.container}>
-      <Header />
-      {console.log(data)}
-      <Main data={data}/>
-      <footer style={{textAlign: 'center'}}>Copyright © 2020 Anran Du | Up-to-date Dec 2020</footer>
-    </div>
-  );
-}
+	useEffect(() => {
+		let url = "./data.json";
+		fetch(url)
+		.then((response) => {return response.json()})
+		.then((data) => setData(data));
+	}, [])
+	
+	return (
+		<div className={css.container}>
+		<Header />
+		{console.log(data)}
+		<Main data={data}/>
+		<footer style={{textAlign: 'center'}}><small>Copyright &copy; {year} Anran Du | Up-to-date {month} {year}</small></footer>
+		</div>
+	);
+	}
 
-export default App;
+	export default App;
